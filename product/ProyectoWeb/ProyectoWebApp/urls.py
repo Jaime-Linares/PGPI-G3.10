dcfba66fb0ff9ec5064c1ee97ad2810cea00bd9f
Jaime-Linares <jailinbar@alum.us.es>
2024-11-09 18:08:37 +0100

    [feat] some changes in home page and "politica privacidad" added

--- product/ProyectoWeb/ProyectoWebApp/urls.py
+++ product/ProyectoWeb/ProyectoWebApp/urls.py
@@ -3,8 +3,10 @@ from ProyectoWebApp import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.home, name="Home"),
+    path('politica-privacidad/', views.politica_privacidad, name="PoliticaPrivacidad"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Para que Django pueda encontrar las imágenes en la carpeta media
--- product/ProyectoWeb/ProyectoWebApp/views.py
+++ product/ProyectoWeb/ProyectoWebApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,HttpResponse
 from carro.carro import Carro
 
-# Create your views here.
+
 
 def home(request):
     carro = Carro(request) # Para que no salga un error del carro en las sesiones, mejor es crear un carro vacío al arrancar la aplicación para que no salga error
     return render(request, "ProyectoWebApp/home.html")
 
 
+def politica_privacidad(request):
+    return render(request, "ProyectoWebApp/politica_privacidad.html")
 
