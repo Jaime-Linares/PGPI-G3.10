ebffdadd668d426d737bac6472626765fafa9702
Jaime-Linares <jailinbar@alum.us.es>
2024-11-09 15:35:02 +0100

    [fix] migration create roles added

--- product/ProyectoWeb/autenticacion/apps.py
+++ product/ProyectoWeb/autenticacion/apps.py
@@ -5,6 +5,3 @@ class AutenticacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'autenticacion'
 
-    def ready(self):
-        import autenticacion.signals
-
