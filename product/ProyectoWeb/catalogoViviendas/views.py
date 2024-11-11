26275ff2e6340bfe94a99c3e62c6c6f8a97f095c
davguifer <davguifer@gmail.com>
2024-11-11 11:45:11 +0100

    [feat] added calendar in detail view

--- product/ProyectoWeb/catalogoViviendas/views.py
+++ product/ProyectoWeb/catalogoViviendas/views.py
@@ -6,8 +6,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Vivienda, Reserva
 from django.contrib.auth.decorators import login_required
-from datetime import datetime
 from django.utils.dateparse import parse_date
+import json
+from datetime import datetime, timedelta
 
 @login_required
 def catalogo_viviendas(request):
@@ -17,16 +18,38 @@ def catalogo_viviendas(request):
     viviendas = Vivienda.objects.all()
     return render(request, "catalogoViviendas/catalogo_viviendas.html", {'viviendas': viviendas,'es_cliente': es_cliente})
 
-@login_required
+
+
+
 def detalle_vivienda(request, id):
     vivienda = get_object_or_404(Vivienda, id=id)
+    reservas = Reserva.objects.filter(vivienda=vivienda)
 
-    es_cliente = request.user.groups.filter(name='Cliente').exists()
-    
-    if not es_cliente:
-        return redirect('Home')
-    
+    fechas_reservadas = [reserva.fecha_inicio.strftime('%Y-%m-%d') for reserva in reservas]
+
     return render(request, "catalogoViviendas/detalle_vivienda.html", {
         'vivienda': vivienda,
-        'es_cliente': es_cliente
+        'fechas_reservadas': json.dumps(fechas_reservadas),
     })
+
+
+def hacer_reserva(request):
+    if request.method == 'POST':
+        vivienda_id = request.POST.get('vivienda_id')
+        fecha_inicio = parse_date(request.POST.get('fecha_inicio'))
+        fecha_fin = parse_date(request.POST.get('fecha_fin'))
+        vivienda = Vivienda.objects.get(id=vivienda_id)
+
+        dias_reserva = (fecha_fin - fecha_inicio).days + 1
+        precio_total = dias_reserva * vivienda.precio_por_dia
+
+        Reserva.objects.create(
+            vivienda=vivienda,
+            usuario=request.user,
+            fecha_inicio=fecha_inicio,
+            fecha_fin=fecha_fin,
+            precio_total=precio_total
+        )
+
+        return JsonResponse({'success': True, 'precio_total': precio_total})
+    return JsonResponse({'success': False})
