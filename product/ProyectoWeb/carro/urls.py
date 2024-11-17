ef3ce9f71bc043fec4274a210dbc18a53c72be64
davguifer <davguifer@gmail.com>
2024-11-17 14:12:46 +0100

    [fix] remove some functions

--- product/ProyectoWeb/carro/urls.py
+++ product/ProyectoWeb/carro/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = "carro"
 
 urlpatterns = [
-    path('reservar/<int:vivienda_id>/', views.reservar_vivienda, name="reservar"),
     path('eliminar/', views.eliminar_reserva, name="eliminar"),
     path('detalle/', views.detalle_carro, name="detalle"),
 ]
--- product/ProyectoWeb/carro/views.py
+++ product/ProyectoWeb/carro/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render, redirect
 from .carro import Carro
-from catalogoViviendas.models import Vivienda
-from catalogoViviendas.forms import ReservaForm
-
-def reservar_vivienda(request, vivienda_id):
-    carro = Carro(request)
-    vivienda = Vivienda.objects.get(id=vivienda_id)
-    carro.agregar(vivienda)
-    return redirect('carro:detalle')
 
 def eliminar_reserva(request):
     carro = Carro(request)
@@ -18,10 +10,4 @@ def detalle_carro(request):
     carro = Carro(request)
     return render(request, 'carro/detalle.html', {'carro': carro})
 
-from django.shortcuts import render
-from carro.carro import Carro
-
-def detalle_carro(request):
-    carro = Carro(request)
-    return render(request, 'carro/detalle.html', {'carro': carro})
 
